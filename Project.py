1dfd222db140d69984fa7bbc38e286d0b46b4777
francesco <francesco.zuppichini@gmail.com>
2019-12-27 17:10:09 +0100

    fixed typo

--- Project.py
+++ Project.py
@@ -5,13 +5,13 @@ from pathlib import Path
 @dataclass
 class Project:
     """
-    This class represents our project. It stores useful information about the structure, e.g. patchs.
+    This class represents our project. It stores useful information about the structure, e.g. paths.
     """
     base_dir: Path = Path(__file__).parents[0]
     data_dir = base_dir / 'dataset'
     checkpoint_dir = base_dir / 'checkpoint'
 
     def __post_init__(self):
-        # create the directory if they does not exist
+        # create the directories if they don't exist
         self.data_dir.mkdir(exist_ok=True)
         self.checkpoint_dir.mkdir(exist_ok=True)
